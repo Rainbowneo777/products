e58a6b3e2e20f626efabd0c6b515f5f0d2e110cd
Rainbowneo777 <80103533+Rainbowneo777@users.noreply.github.com>
2021-05-14 20:27:57 +0800

    Add files via upload

--- products_txt.py
+++ products_txt.py
@@ -2,9 +2,12 @@
 products = []
 with open('products.csv', 'r') as f:
 	for line in f:
-		line.strip().split(',')
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
 
-
+print(products)
 
 
 
